a0ac08971d6108a35d896df85b5810514e4f9fa7
JohnM <zhekamol@gmail.com>
2020-11-12 18:15:25 +0300

    MSVC fix

--- common.cmake
+++ common.cmake
@@ -10,6 +10,10 @@ macro(general_params)
 	set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 	set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+
+	if(MSVC)
+		add_compile_options("/Zc:preprocessor")
+	endif()
 endmacro()
 
 macro(target_install)
