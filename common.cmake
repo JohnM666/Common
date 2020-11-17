5d4dd0917eaf04b3744d7f0e8e9351cbb4844ee0
JohnM <zhekamol@gmail.com>
2020-11-18 02:05:27 +0300

    build_externals fix

--- common.cmake
+++ common.cmake
@@ -177,15 +177,15 @@ endfunction()
 
 function(build_externals)
 	configure_file(external/CMakeLists.txt external/CMakeLists.txt)
-	execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+	execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" "${CMAKE_CURRENT_BINARY_DIR}/external"
   		RESULT_VARIABLE result
-  		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/external/)
+  		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/external/)
 	if(result)
   		message(FATAL_ERROR "CMake step for externals failed: ${result}")
 	endif()
-	execute_process(COMMAND ${CMAKE_COMMAND} --build .
+	execute_process(COMMAND ${CMAKE_COMMAND} --build "${CMAKE_CURRENT_BINARY_DIR}/external"
   		RESULT_VARIABLE result
-  		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/external/)
+  		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/external/)
 	if(result)
   		message(FATAL_ERROR "Build step for externals failed: ${result}")
 	endif()
