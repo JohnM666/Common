21cca05f9b646aea8111ac3b20942c2f4963ac4e
JohnM <zhekamol@gmail.com>
2020-12-06 22:21:50 +0300

    Fixed pyhton binding

--- common.cmake
+++ common.cmake
@@ -158,7 +158,7 @@ function(target_reflect target apiDef pythonBindingDirectory)
 	get_target_property(target_type ${PROJECT_NAME} TYPE)
 
 	if(NOT target_type STREQUAL "EXECUTABLE")
-        	file(GLOB PYTHON_SOURCES {pythonBindingDirectory})
+        	file(GLOB PYTHON_SOURCES "{pythonBindingDirectory}/*.cpp")
         	pybind11_add_module(${PROJECT_NAME}Py ${PYTHON_SOURCES})
         	target_link_libraries(${PROJECT_NAME}Py PRIVATE ${PROJECT_NAME})
 	endif()
